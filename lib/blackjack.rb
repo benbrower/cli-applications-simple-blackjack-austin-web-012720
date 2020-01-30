a3909297da5c7d1b7f25f07eb30f94c9c1ebaaa0
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-30 02:51:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,7 @@ end
 def initial_round
   # code #initial_round here
   card_total = 0
-  card total = card_total + deal_card
+  card_total = card_total + deal_card
   card_total = card_total + deal_card
   display_card_total(card_total)
   card_total
